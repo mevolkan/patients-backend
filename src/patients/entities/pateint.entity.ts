81ed867f9523898dec293869b03b4860baa92ed8
volkan <nzaro19@gmail.com>
2023-11-14 15:08:01 +0300

    model patient entity

--- src/patients/entities/pateint.entity.ts
+++ src/patients/entities/pateint.entity.ts
@@ -1,1 +1,19 @@
-export class Pateint {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Patient {
+  @PrimaryGeneratedColumn()
+  patientId: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  dob: Date;
+
+  @Column()
+  gender: string;
+}
